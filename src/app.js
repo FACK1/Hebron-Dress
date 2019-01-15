7d65c3b72e5c74da4632234a2ac9d30af412b218
Amin <ayman201266@hotmail.com>
2019-01-15 16:00:33 +0200

    build search function that return dresses #28

--- src/app.js
+++ src/app.js
@@ -1,9 +1,13 @@
-
 const express = require('express');
 const path = require('path');
 const exphbs = require('express-handlebars');
 
+// const availableDresses = require('./database/queries/getDresses.js');
+
+const { Dress, } = require('./database/models/');
+
 const app = express();
+
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
@@ -16,5 +20,22 @@ app.engine(
     defaultLayout: 'main',
   })
 );
+
+
+app.get('/search', (req, res) => {
+  const { color, } = req.query;
+  let selected = color;
+  if (color === 'color') {
+    selected = { $like: '%%', };
+  } Dress.findAll(
+    {
+      where: { color: selected, },
+    }
+  ).then((dresses) => {
+    res.send(dresses);
+  }).catch((error) => {
+    res.send(error);
+  });
+});
 
 module.exports = app;
