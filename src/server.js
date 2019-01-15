6765f657b467b7e9c59de764ea35448267d58f62
safaaamro <safaaamro072@gmail.com>
2019-01-15 13:36:51 +0200

    design home page update #15

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 const app = require('./app.js');
 
-const port = process.env.PORT || 3008;
+const port = process.env.PORT || 3009;
 app.listen(port, () => {
   console.log(`Our app is running on http://localhost:${port}`);
 });
