7253ce0cd3d809b2be63c37bb2364441d3dbc4f2
shaima96 <141062@ppu.edu.ps>
2019-01-17 07:59:30 -0600

    create profil page and adding page

---
+++ src/controllers/add.js
@@ -0,0 +1,4 @@
+const add = require('../database/models');
+const jwt = require('jsonwebtoken');
+const  cookie = require('cookie');
+const {	SECRET } = process.env
--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const error = require('./error');
 const home = require('./home');
+const profile = require('./profile');
+const add = require('./add');
+
 
 const router = express.Router();
 
 router.get('/', home.get);
+router.get('/profile', profile.get);
+router.post('/add', add.post);
 
 router.use(error.pageNotFound);
 router.use(error.serverError);
