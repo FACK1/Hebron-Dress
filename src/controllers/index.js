fd17b9dd3d23b8cc0d78354b744ff15582994db2
razantayyem <razanr543@gmail.com>
2019-01-21 09:47:23 +0200

    page one dress

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -3,13 +3,10 @@ const error = require('./error');
 const home = require('./home');
 const oneDress = require('./oneDress');
 
-
 const router = express.Router();
 
 router.get('/', home.get);
 router.get('/oneDress/:id', oneDress.get);
-
-
 router.use(error.pageNotFound);
 router.use(error.serverError);
 module.exports = router;
