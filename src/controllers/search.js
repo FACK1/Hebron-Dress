0afb796fa609ecf0703bf9ec2ed3a908290d6fa1
Amin <ayman201266@hotmail.com>
2019-01-21 11:47:51 +0200

    finish search endpoint #18

--- src/controllers/search.js
+++ src/controllers/search.js
@@ -1,20 +1,31 @@
-
+const sequelize = require('sequelize');
 const { Dress, } = require('../database/models');
 
-exports.get('/search', (req, res, next) => {
+const Op = sequelize.Op;
+
+exports.get = (req, res, next) => {
 // search query
 
-  const { color, size, status, } = req.query;
+  const {
+    color, size, status, category, price,
+  } = req.query;
+  console.log(price);
   let selected = color;
   let sizeSel = size;
   let for_rent; let for_sale;
   let available;
+  let categorySel = category;
+  let priceSel = price;
   let query = {};
   if (color === 'color') {
     selected = { $like: '%%', };
   } if (size === 'size') {
     sizeSel = { $like: '%%', };
   }
+  if (category === 'category') {
+    categorySel = { $like: '%%', };
+  }
+
   if (status === 'rent') {
     for_rent = true;
     for_sale = false;
@@ -30,16 +41,32 @@ exports.get('/search', (req, res, next) => {
     for_sale = true;
     available = true;
   }
+
+
+  const min = price.split('-')[0];
+  const max = price.split('-')[1];
+
+  if (max < 1000) {
+    priceSel = { [Op.between]: [min, max,], };
+  } else if (price === 'price') {
+    priceSel = { [Op.gte]: 0, };
+  } else {
+    priceSel = { [Op.gte]: 1000, };
+  }
+
+
   if (status === 'status') {
-    query = { color: selected, size: sizeSel, };
+    query = {
+      color: selected, size: sizeSel, category: categorySel, price: priceSel,
+    };
   } else {
     query = {
-      color: selected, size: sizeSel, for_rent, for_sale, available,
+      color: selected, size: sizeSel, for_rent, for_sale, available, category: categorySel, priceSel: price,
     };
   }
   Dress.findAll({ where: query, }).then((dresses) => {
-    res.render('product', { dresses, });
+    res.render('product', { dresses, css: 'product', });
   }).catch((error) => {
     next(error);
   });
-});
+};
