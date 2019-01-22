1035d3d284de0f0c10c7cf2d4eee5f03e0446f2b
safaaamro <safaaamro072@gmail.com>
2019-01-22 08:57:20 +0200

    make frontend page contact

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -3,12 +3,14 @@ const error = require('./error');
 const home = require('./home');
 const oneDress = require('./oneDress');
 const products = require('./products');
+const contact = require('./contact');
 
 const router = express.Router();
 
 router.get('/', home.get);
 router.get('/oneDress/:id', oneDress.get);
 router.get('/products', products.get);
+router.get('/contact', contact.get);
 router.use(error.pageNotFound);
 router.use(error.serverError);
 module.exports = router;
